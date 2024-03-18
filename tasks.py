4e7ae43c3da142d95f94d5e92add03c0d3922513
HackXIt <hackxit@gmail.com>
2024-03-19 00:30:07 +0100

    Improve tasks

--- tasks.py
+++ tasks.py
@@ -6,6 +6,9 @@ lv_micropython_dir = os.path.join(os.path.curdir, 'lv_micropython')
 micropython = os.path.join(os.path.curdir, 'lv_micropython', 'ports', 'unix', 'build-standard', 'micropython')
 main = os.path.join(os.path.curdir, 'src', 'main.py')
 jpg_conversion = os.path.join(os.path.curdir, 'src', 'jpg_conversion.py')
+lv_conf_project = os.path.join(os.path.curdir, 'lv_conf.h')
+lv_conf_original = os.path.join(os.path.curdir, 'lv_micropython', 'lib', 'lv_bindings', 'lv_conf.h')
+lv_conf_temp = os.path.join(os.path.curdir, 'lv_conf.tmp')
 
 @task
 def generate(ctx):
@@ -15,7 +18,11 @@ def generate(ctx):
 @task
 def sample(ctx, type='button', count='1', width='420', height='320', layout='none'):
     subprocess.run([micropython, main, '-W', width, '-H', height, '-c', count, '-t', type, '-o', 'screenshot.bin', '-l', layout])
-    subprocess.run(['poetry', 'run', 'python', jpg_conversion])
+    subprocess.run(['poetry', 'run', 'python', jpg_conversion, '-W', width, '-H', height, '-i', 'screenshot.bin', '-o', 'screenshot.jpg'])
+
+@task
+def clear(ctx):
+    subprocess.run(['rm', 'screenshot.raw', 'screenshot.bin', 'screenshot.jpg'])
 
 @task
 def test(ctx):
@@ -23,6 +30,12 @@ def test(ctx):
 
 @task
 def build(ctx):
-    subprocess.run(['make', '-C', 'mpy-cross'], cwd=lv_micropython_dir)
-    subprocess.run(['make', '-C', 'ports/unix', 'submodules'], cwd=lv_micropython_dir)
-    subprocess.run(['make', '-C', 'ports/unix'], cwd=lv_micropython_dir)
+    # Move lv_conf.h to a temporary location
+    subprocess.run(['mv', lv_conf_original, lv_conf_temp])
+    # Override lv_conf.h
+    subprocess.run(['cp', lv_conf_project, lv_conf_original])
+    subprocess.run(['make', '-j4', '-C', 'mpy-cross'], cwd=lv_micropython_dir)
+    subprocess.run(['make', '-j4', '-C', 'ports/unix', 'submodules'], cwd=lv_micropython_dir)
+    subprocess.run(['make', '-j4', '-C', 'ports/unix'], cwd=lv_micropython_dir)
+    # Restore lv_conf.h so that git doesn't complain about dirty changes
+    subprocess.run(['mv', lv_conf_temp, lv_conf_original])
