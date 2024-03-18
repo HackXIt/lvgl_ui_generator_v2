e6af1177267f1286eddc97a966bb410e532561bf
HackXIt <hackxit@gmail.com>
2024-03-19 00:48:29 +0100

    Add copy after build to .venv bin folder for ease-of-use (on successful build)

--- tasks.py
+++ tasks.py
@@ -34,8 +34,16 @@ def build(ctx):
     subprocess.run(['mv', lv_conf_original, lv_conf_temp])
     # Override lv_conf.h
     subprocess.run(['cp', lv_conf_project, lv_conf_original])
-    subprocess.run(['make', '-j4', '-C', 'mpy-cross'], cwd=lv_micropython_dir)
-    subprocess.run(['make', '-j4', '-C', 'ports/unix', 'submodules'], cwd=lv_micropython_dir)
-    subprocess.run(['make', '-j4', '-C', 'ports/unix'], cwd=lv_micropython_dir)
+    mpy_cross = subprocess.run(['make', '-j4', '-C', 'mpy-cross'], cwd=lv_micropython_dir, capture_output=True)
+    unix_port_submodules = subprocess.run(['make', '-j4', '-C', 'ports/unix', 'submodules'], cwd=lv_micropython_dir, capture_output=True)
+    unix_port = subprocess.run(['make', '-j4', '-C', 'ports/unix'], cwd=lv_micropython_dir, capture_output=True)
     # Restore lv_conf.h so that git doesn't complain about dirty changes
-    subprocess.run(['mv', lv_conf_temp, lv_conf_original])
+    subprocess.run(['mv', lv_conf_temp, lv_conf_original])    
+    if mpy_cross.returncode == 0 and unix_port_submodules.returncode == 0 and unix_port.returncode == 0:
+        # Copy the micropython binary to the root .venv directory
+        subprocess.run(['cp', micropython, os.path.join(os.path.curdir, '.venv', 'bin', 'micropython')])
+    else:
+        print("Build failed.")
+        print(mpy_cross.stderr)
+        print(unix_port_submodules.stderr)
+        print(unix_port.stderr)
