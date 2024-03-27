9dc846edef4830a8d2d5413dfe7c143ed672b6cf
HackXIt <hackxit@gmail.com>
2024-03-27 23:54:28 +0100

    Refactor code, reordering for readability, functionalize widget creation

--- src/design_parser.py
+++ src/design_parser.py
@@ -6,7 +6,9 @@ import random
 ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 class UiLoader:
-    valid_types = ["container", "label", "button", "image", "line", "bar", "slider", "switch", "checkbox", "roller", "dropdown", "textarea", "chart", "random"]
+    special_types = ["container", "random"]
+    widget_types = ["arc", "bar", "button", "buttonmatrix", "calendar", "chart", "checkbox", "dropdown", "image", "imagebutton", "keyboard", "label", "led", "line", "list", "menu", "messagebox", "roller", "scale", "slider", "spangroup", "spinbox", "spinner", "switch", "table", "tabview", "textarea", "tileview", "window"]
+    valid_types = special_types + widget_types
     def __init__(self, json_file):
         # Load JSON data from file
         with open(json_file, "r") as file:
@@ -45,8 +47,6 @@ class UiLoader:
             self.apply_style(widget, element["style"])
         return widget
 
-# NOTE ------------ WIDGET CREATION METHODS ------------
-
     def create_widget(self, element) -> lv.obj | lv.button | lv.label | lv.image | lv.line | lv.bar | lv.slider | lv.switch | lv.checkbox | lv.roller | lv.dropdown | lv.textarea | lv.chart:
         widget = None
         widget_type = element["type"]
@@ -54,43 +54,66 @@ class UiLoader:
             raise ValueError(f"Invalid widget type: {widget_type}, valid types are: {self.valid_types}")
         if widget_type == 'container':
             widget = self.create_container(element)
-        elif widget_type == 'button':
-            widget = self.create_button(element)
-        elif widget_type == 'label':
-            widget = self.create_label(element)
-        elif widget_type == 'image':
-            widget = self.create_image(element)
-        elif widget_type == 'line':
-            widget = self.create_line(element)
+        elif widget_type == 'arc':
+            widget = self.create_arc(element)
         elif widget_type == 'bar':
             widget = self.create_bar(element)
-        elif widget_type == 'slider':
-            widget = self.create_slider(element)
-        elif widget_type == 'dropdown':
-            widget = self.create_dropdown(element)
+        elif widget_type == 'button':
+            widget = self.create_button(element)
+        elif widget_type == 'buttonmatrix':
+            widget = self.create_buttonmatrix(element)
+        elif widget_type == 'calendar':
+            widget = self.create_calendar(element)
         elif widget_type == 'chart':
             widget = self.create_chart(element)
-        elif widget_type == 'switch':
-            widget = self.create_switch(element)
         elif widget_type == 'checkbox':
             widget = self.create_checkbox(element)
+        elif widget_type == 'dropdown':
+            widget = self.create_dropdown(element)
+        elif widget_type == 'image':
+            widget = self.create_image(element)
+        elif widget_type == 'imagebutton':
+            widget = self.create_imagebutton(element)
+        elif widget_type == 'keyboard':
+            widget = self.create_keyboard(element)
+        elif widget_type == 'label':
+            widget = self.create_label(element)
+        elif widget_type == 'led':
+            widget = self.create_led(element)
+        elif widget_type == 'line':
+            widget = self.create_line(element)
+        elif widget_type == 'list':
+            widget = self.create_list(element)
+        elif widget_type == 'menu':
+            widget = self.create_menu(element)
+        elif widget_type == 'messagebox':
+            widget = self.create_messagebox(element)
         elif widget_type == 'roller':
             widget = self.create_roller(element)
+        elif widget_type == 'scale':
+            widget = self.create_scale(element)
+        elif widget_type == 'slider':
+            widget = self.create_slider(element)
+        elif widget_type == 'spangroup':
+            widget = self.create_spangroup(element)
+        elif widget_type == 'spinbox':
+            widget = self.create_spinbox(element)
+        elif widget_type == 'spinner':
+            widget = self.create_spinner(element)
+        elif widget_type == 'switch':
+            widget = self.create_switch(element)
+        elif widget_type == 'table':
+            widget = self.create_table(element)
+        elif widget_type == 'tabview':
+            widget = self.create_tabview(element)
         elif widget_type == 'textarea':
             widget = self.create_textarea(element)
+        elif widget_type == 'tileview':
+            widget = self.create_tileview(element)
+        elif widget_type == 'window':
+            widget = self.create_window(element)
         elif widget_type == "random":
-            # FIXME Placement of random widget is not handled properly (inside grid layout)
-            if "parent_id" not in element:
-                raise ValueError(f"Random widget must have parent_id: {element}")
-            if "count" not in element:
-                raise ValueError(f"Random widget must have count: {element}")
-            if "count" in element:
-                for i in range(element["count"]):
-                    element["type"] = random.choice(element["options"])
-                    widget = self.create_widget(element)
-                    widget.set_parent(self.widgets[element["parent_id"]])
-                    if "style" in element:
-                        self.apply_style(widget, element["style"])
+            widget = self.create_random_widget(element)
         if widget == None:
             raise ValueError(f"Failed to create widget: {element}")
         # Create a unique ID for the widget if it wasn't provided
@@ -103,7 +126,9 @@ class UiLoader:
         self.widgets[element["id"]] = widget
         print(f"Created widget '{element['id']}': {element}")
         return widget
-    
+
+# NOTE ------------ SPECIAL CREATION METHODS ------------
+
     def create_container(self, element) -> lv.obj:
         if "options" not in element:
             raise ValueError(f"Container widget must have options: {element}")
@@ -149,6 +174,38 @@ class UiLoader:
         else:
             container.set_grid_dsc_array([1, 1], [1, 1])
     
+    def create_random_widget(self, element):
+        # FIXME Placement of random widget is not handled properly (inside grid layout)
+        if "parent_id" not in element:
+            raise ValueError(f"Random widget must have parent_id: {element}")
+        if "count" not in element:
+            raise ValueError(f"Random widget must have count: {element}")
+        if "count" in element:
+            for i in range(element["count"]):
+                element["type"] = random.choice(element["options"])
+                widget = self.create_widget(element)
+                widget.set_parent(self.widgets[element["parent_id"]])
+                if "style" in element:
+                    self.apply_style(widget, element["style"])
+        return widget
+
+# NOTE ------------ WIDGET CREATION METHODS ------------
+
+    def create_arc(self, element) -> lv.arc:
+        # TODO Implement arc widget
+        widget = lv.arc(lv.screen_active())
+        return widget
+    
+    def create_bar(self, element) -> lv.bar:
+        widget = lv.bar(lv.screen_active())
+        if "options" in element:
+            range_min = element["options"].get("range_min", 0)
+            range_max = element["options"].get("range_max", 100)
+            value = element["options"].get("value", 0)
+            widget.set_range(range_min, range_max)
+            widget.set_value(value, lv.ANIM.ON)
+        return widget
+    
     def create_button(self, element) -> lv.button:
         # FIXME Label of button currently ignores style properties (uses defaults)
         widget = lv.button(lv.screen_active())
@@ -159,59 +216,17 @@ class UiLoader:
             element["text"] = text
         label.set_text(element["text"])
         return widget
-
-    def create_label(self, element) -> lv.label:
-        widget = lv.label(lv.screen_active())
-        if "text" not in element:
-            text = "".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))])
-            element["text"] = text
-        widget.set_text(element["text"])
+    
+    def create_buttonmatrix(self, element) -> lv.buttonmatrix:
+        # TODO Implement buttonmatrix widget
+        widget = lv.buttonmatrix(lv.screen_active())
         return widget
-
-    def create_image(self, element) -> lv.image:
-        # TODO Implement image widget
-        widget = lv.image(lv.screen_active())
-        # widget.set_src(lv.SYMBOL.OK)
+    
+    def create_calendar(self, element) -> lv.calendar:
+        # TODO Implement calendar widget
+        widget = lv.calendar(lv.screen_active())
         return widget
-
-    def create_line(self, element) -> lv.line:
-        # TODO Implement line widget (also tough to implement)
-        widget = lv.line(lv.screen_active())
-        # if "options" in element:
-        #     points = element["options"].get("points", [])
-        #     widget.set_points(points)
-        return widget
-
-    def create_bar(self, element) -> lv.bar:
-        widget = lv.bar(lv.screen_active())
-        if "options" in element:
-            range_min = element["options"].get("range_min", 0)
-            range_max = element["options"].get("range_max", 100)
-            value = element["options"].get("value", 0)
-            widget.set_range(range_min, range_max)
-            widget.set_value(value, lv.ANIM.ON)
-        return widget
-
-    def create_slider(self, element) -> lv.slider:
-        widget = lv.slider(lv.screen_active())
-        if "options" in element:
-            range_min = element["options"].get("range_min", 0)
-            range_max = element["options"].get("range_max", 100)
-            value = element["options"].get("value", 0)
-            widget.set_range(range_min, range_max)
-            widget.set_value(value, lv.ANIM.ON)
-        return widget
-
-    def create_dropdown(self, element) -> lv.dropdown:
-        widget = lv.dropdown(lv.screen_active())
-        if "options" in element:
-            options = element["options"].get("options", [])
-            if len(options) == 0:
-                for i in range(random.randint(1, 10)):
-                    options.append("".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))]))
-            widget.set_options("\n".join(options))
-        return widget
-
+    
     def create_chart(self, element) -> lv.chart:
         # TODO Implement chart widget (also tough to implement)
         widget = lv.chart(lv.screen_active())
@@ -226,18 +241,80 @@ class UiLoader:
         #         points = series_option.get("points", [])
         #         widget.set_points(series, points)
         return widget
-
-    def create_switch(self, element) -> lv.switch:
-        # TODO Implement switch widget
-        widget = lv.switch(lv.screen_active())
+    
+    def create_canvas(self, element) -> lv.canvas:
+        # TODO Implement canvas widget
+        widget = lv.canvas(lv.screen_active())
         return widget
-
+    
     def create_checkbox(self, element) -> lv.checkbox:
         # TODO Implement checkbox widget
         widget = lv.checkbox(lv.screen_active())
         # widget.set_text('Checkbox')
         return widget
+    
+    def create_dropdown(self, element) -> lv.dropdown:
+        widget = lv.dropdown(lv.screen_active())
+        if "options" in element:
+            options = element["options"].get("options", [])
+            if len(options) == 0:
+                for i in range(random.randint(1, 10)):
+                    options.append("".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))]))
+            widget.set_options("\n".join(options))
+        return widget
 
+    def create_image(self, element) -> lv.image:
+        # TODO Implement image widget
+        widget = lv.image(lv.screen_active())
+        # widget.set_src(lv.SYMBOL.OK)
+        return widget
+    
+    def create_imagebutton(self, element) -> lv.imagebutton:
+        # TODO Implement image_button widget
+        widget = lv.imagebutton(lv.screen_active())
+        return widget
+    
+    def create_keyboard(self, element) -> lv.keyboard:
+        # TODO Implement keyboard widget
+        widget = lv.keyboard(lv.screen_active())
+        return widget
+
+    def create_label(self, element) -> lv.label:
+        widget = lv.label(lv.screen_active())
+        if "text" not in element:
+            text = "".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))])
+            element["text"] = text
+        widget.set_text(element["text"])
+        return widget
+    
+    def create_led(self, element) -> lv.led:
+        # TODO Implement led widget
+        widget = lv.led(lv.screen_active())
+        return widget
+
+    def create_line(self, element) -> lv.line:
+        # TODO Implement line widget (also tough to implement)
+        widget = lv.line(lv.screen_active())
+        # if "options" in element:
+        #     points = element["options"].get("points", [])
+        #     widget.set_points(points)
+        return widget
+    
+    def create_list(self, element) -> lv.list:
+        # TODO Implement list widget
+        widget = lv.list(lv.screen_active())
+        return widget
+    
+    def create_menu(self, element) -> lv.menu:
+        # TODO Implement menu widget
+        widget = lv.menu(lv.screen_active())
+        return widget
+    
+    def create_messagebox(self, element) -> lv.msgbox:
+        # TODO Implement messagebox widget
+        widget = lv.msgbox(lv.screen_active())
+        return widget
+    
     def create_roller(self, element) -> lv.roller:
         # TODO Implement roller widget
         widget = lv.roller(lv.screen_active())
@@ -245,12 +322,68 @@ class UiLoader:
         #     options = '\n'.join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))])
         # widget.set_options(options)
         return widget
+    
+    def create_scale(self, element) -> lv.scale:
+        # TODO Implement scale widget
+        widget = lv.scale(lv.screen_active())
+        return widget
+
+    def create_slider(self, element) -> lv.slider:
+        widget = lv.slider(lv.screen_active())
+        if "options" in element:
+            range_min = element["options"].get("range_min", 0)
+            range_max = element["options"].get("range_max", 100)
+            value = element["options"].get("value", 0)
+            widget.set_range(range_min, range_max)
+            widget.set_value(value, lv.ANIM.ON)
+        return widget
+
+    def create_spangroup(self, element) -> lv.spangroup:
+        # TODO Implement span widget
+        widget = lv.spangroup(lv.screen_active())
+        return widget
+
+    def create_spinbox(self, element) -> lv.spinbox:
+        # TODO Implement spinbox widget
+        widget = lv.spinbox(lv.screen_active())
+        return widget
+
+    def create_spinner(self, element) -> lv.spinner:
+        # TODO Implement spinner widget
+        widget = lv.spinner(lv.screen_active())
+        return widget
+
+    def create_switch(self, element) -> lv.switch:
+        # TODO Implement switch widget
+        widget = lv.switch(lv.screen_active())
+        return widget
+    
+    def create_table(self, element) -> lv.table:
+        # TODO Implement table widget
+        widget = lv.table(lv.screen_active())
+        return widget
+
+    def create_tabview(self, element) -> lv.tabview:
+        # TODO Implement tabview widget
+        widget = lv.tabview(lv.screen_active())
+        return widget
 
     def create_textarea(self, element) -> lv.textarea:
         # TODO Implement textarea widget
         widget = lv.textarea(lv.screen_active())
         # widget.set_text('Textarea')
         return widget
+    
+    def create_tileview(self, element) -> lv.tileview:
+        # TODO Implement tileview widget
+        widget = lv.tileview(lv.screen_active())
+        return widget
+    
+    def create_window(self, element) -> lv.win:
+        # TODO Implement window widget
+        widget = lv.win(lv.screen_active())
+        return widget
+
     
 # NOTE ------------ STYLE CREATION METHODS ------------
     
