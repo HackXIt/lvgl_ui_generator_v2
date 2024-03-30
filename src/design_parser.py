639365255964398732b227f3fb65d1a7cffa37de
HackXIt <hackxit@gmail.com>
2024-03-30 16:20:45 +0100

    Implement a few more widgets with options or random values

--- src/design_parser.py
+++ src/design_parser.py
@@ -11,6 +11,7 @@ class UiLoader:
     valid_types = special_types + widget_types
     valid_layouts = ["none", "grid", "flex"]
     valid_flow = ["row", "column", "row_wrap", "column_wrap", "row_reverse", "column_reverse"]
+    _options = "options"
     def __init__(self, json_file):
         # Load JSON data from file
         with open(json_file, "r") as file:
@@ -204,8 +205,27 @@ class UiLoader:
 # NOTE ------------ WIDGET CREATION METHODS ------------
 
     def create_arc(self, element) -> lv.arc:
-        # TODO Implement arc widget
         widget = lv.arc(lv.screen_active())
+        if "options" in element:
+            mode = element["options"].get("mode", "normal")
+            arc_mode = getattr(lv.arc.MODE, mode.upper(), lv.arc.MODE.NORMAL)
+            range_max = element["options"].get("range_max", random.randint(1, 100))
+            range_min = element["options"].get("range_min", random.randint(1, range_max))
+            value = element["options"].get("value", random.randint(range_min, range_max))
+            rotation = element["options"].get("rotation", 0)
+            angle_start, angle_end = element["options"].get("angle_range", (0, 360))
+            widget.set_mode(arc_mode)
+            widget.set_angles(angle_start, angle_end)   
+            widget.set_rotation(rotation)
+            widget.set_range(range_min, range_max)
+            widget.set_value(value)
+        else:
+            range_max = random.randint(0, 100)
+            range_min = random.randint(0, range_max)
+            # NOTE Leave other arc properties at default
+            widget.set_mode(lv.arc.MODE.NORMAL)
+            widget.set_range(range_min, range_max)
+            widget.set_value(random.randint(range_min, range_max))
         return widget
     
     def create_bar(self, element) -> lv.bar:
@@ -213,30 +233,67 @@ class UiLoader:
         if "options" in element:
             range_min = element["options"].get("range_min", 0)
             range_max = element["options"].get("range_max", 100)
-            value = element["options"].get("value", 0)
+            value = element["options"].get("value", random.randint(range_min, range_max))
             widget.set_range(range_min, range_max)
             widget.set_value(value, lv.ANIM.ON)
+        else:
+            widget.set_range(0, 100)
+            widget.set_value(random.randint(0, 100), lv.ANIM.ON)
         return widget
     
     def create_button(self, element) -> lv.button:
-        # FIXME Label of button currently ignores style properties (uses defaults)
         widget = lv.button(lv.screen_active())
-        label = lv.label(widget)
+        label = lv.label(widget) # FIXME Label of button currently ignores style properties (uses defaults)
         label.set_parent(widget)
-        if "text" not in element:
+        if "options" in element:
+            text = element["options"].get("text", "".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))]))
+        else:
             text = "".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))])
-            element["text"] = text
-        label.set_text(element["text"])
+        label.set_text(text)
         return widget
     
     def create_buttonmatrix(self, element) -> lv.buttonmatrix:
-        # TODO Implement buttonmatrix widget
         widget = lv.buttonmatrix(lv.screen_active())
+        if "options" in element:
+            map = element["options"].get("map", [random.choice(ascii_letters) for _ in range(random.randint(1, 10))])
+            widget.set_map(map)
+        else:
+            widget.set_map([random.choice(ascii_letters) for _ in range(random.randint(1, 10))])
         return widget
     
     def create_calendar(self, element) -> lv.calendar:
-        # TODO Implement calendar widget
         widget = lv.calendar(lv.screen_active())
+        if "options" in element:
+            current_date = element["options"].get("current_date", {"year": 2024, "month": 3, "day": 30})
+            showed_date = element["options"].get("showed_date", {"year": 2024, "month": 3})
+            if "year" not in current_date or "month" not in current_date or "day" not in current_date:
+                raise ValueError(f"Invalid date format for current_date: {current_date}. Must have 'year', 'month', 'day' keys.")
+            if "year" not in showed_date or "month" not in showed_date:
+                raise ValueError(f"Invalid date format for showed_date: {showed_date}. Must have 'year', 'month' keys.")
+            date_highlights = element["options"].get("date_highlights", [])
+            widget.set_today_date(current_date.year, current_date.month, current_date.day)
+            widget.set_showed_date(showed_date.year, showed_date.month)
+            if len(date_highlights) > 0:
+                if type(date_highlights) is not list:
+                    raise ValueError(f"Invalid date_highlights format: {date_highlights}. Must be a list.")
+                highlights = []
+                for date_highlight in date_highlights:
+                    if "year" not in date_highlight or "month" not in date_highlight or "day" not in date_highlight:
+                        raise ValueError(f"Invalid date format for date_highlight: {date_highlight}. Must have 'year', 'month', 'day' keys.")
+                    date = lv.calendar_date_t()
+                    date.year = date_highlight.year
+                    date.month = date_highlight.month
+                    date.day = date_highlight.day
+                    highlights.append(date)
+                widget.set_highlighted_dates(highlights, len(highlights))
+        else:
+            widget.set_today_date(2024, 3, 30)
+            widget.set_showed_date(2024, 3)
+            highlight = lv.calendar_date_t()
+            highlight.year = 2024
+            highlight.month = 3
+            highlight.day = 29
+            widget.set_highlighted_dates([highlight], 1)
         return widget
     
     def create_chart(self, element) -> lv.chart:
@@ -262,17 +319,22 @@ class UiLoader:
     def create_checkbox(self, element) -> lv.checkbox:
         # TODO Implement checkbox widget
         widget = lv.checkbox(lv.screen_active())
-        # widget.set_text('Checkbox')
+        if "options" in element:
+            state = element["options"].get("state", "disabled")
+            cb_state = getattr(lv.STATE, state.upper(), lv.STATE.DISABLED)
+            widget.add_state(cb_state)
+        else:
+            widget.add_state(lv.STATE.DEFAULT)
         return widget
     
     def create_dropdown(self, element) -> lv.dropdown:
         widget = lv.dropdown(lv.screen_active())
         if "options" in element:
-            options = element["options"].get("options", [])
-            if len(options) == 0:
+            entries = element["options"].get("entries", [])
+            if len(entries) == 0:
                 for i in range(random.randint(1, 10)):
-                    options.append("".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))]))
-            widget.set_options("\n".join(options))
+                    entries.append("".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))]))
+            widget.set_options("\n".join(entries))
         return widget
 
     def create_image(self, element) -> lv.image:
@@ -293,15 +355,21 @@ class UiLoader:
 
     def create_label(self, element) -> lv.label:
         widget = lv.label(lv.screen_active())
-        if "text" not in element:
+        if "options" in element:
+            text = element["options"].get("text", "".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))]))
+        else:
             text = "".join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))])
-            element["text"] = text
-        widget.set_text(element["text"])
+        widget.set_text(text)
         return widget
     
     def create_led(self, element) -> lv.led:
         # TODO Implement led widget
         widget = lv.led(lv.screen_active())
+        if "options" in element:
+            brightness = element["options"].get("brightness", 100)
+            widget.set_brightness(brightness)
+        else:
+            widget.set_brightness(random.randint(0, 100))
         return widget
 
     def create_line(self, element) -> lv.line:
@@ -328,16 +396,45 @@ class UiLoader:
         return widget
     
     def create_roller(self, element) -> lv.roller:
-        # TODO Implement roller widget
         widget = lv.roller(lv.screen_active())
-        # for i in range(random.randint(1, 10)):
-        #     options = '\n'.join([random.choice(ascii_letters) for _ in range(random.randint(1, 10))])
-        # widget.set_options(options)
+        if "options" in element:
+            entries = element["options"].get("entries", [random.choice(ascii_letters) for _ in range(1, random.randint(1, 10))])
+            mode = element["options"].get("mode", "infinite")
+            visible_rows = element["options"].get("visible_rows", 3)
+            roller_mode = getattr(lv.roller.MODE, mode.upper(), lv.roller.MODE.INFINITE)
+            widget.set_options("\n".join(entries), roller_mode)
+            widget.set_visible_row_count(visible_rows)
+        else:
+            widget.set_options("\n".join([random.choice(ascii_letters) for _ in range(1, random.randint(1, 10))]), lv.roller.MODE.INFINITE)
         return widget
     
     def create_scale(self, element) -> lv.scale:
         # TODO Implement scale widget
         widget = lv.scale(lv.screen_active())
+        if "options" in element:
+            mode = element["options"].get("mode", "horizontal_bottom")
+            scale_mode = getattr(lv.scale.MODE, mode.upper(), lv.scale.MODE.HORIZONTAL_BOTTOM)
+            show_label = element["options"].get("show_label", True)
+            total_ticks = element["options"].get("total_ticks", random.randint(1, 100))
+            major_ticks = element["options"].get("major_ticks", random.randint(1, total_ticks))
+            major_range = element["options"].get("major_range", random.randint(1, 100))
+            minor_range = element["options"].get("minor_range", random.randint(1, major_range))
+            sections = element["options"].get("sections", random.randint(1, 10))
+            default_labels = [x for x in range(sections) for _ in range(random.randint(1, 10)) for c in ascii_letters]
+            labels = element["options"].get("labels", default_labels)
+            widget.set_mode(scale_mode)
+            widget.set_range(minor_range, major_range)
+            widget.set_total_tick_count(total_ticks)
+            widget.set_major_tick_every(major_ticks)
+            widget.set_label_show(show_label)
+            widget.set_text_src(labels)
+        else:
+            widget.set_mode(lv.scale.MODE.HORIZONTAL_BOTTOM)
+            widget.set_range(0, 100)
+            widget.set_total_tick_count(random.randint(1, 100))
+            widget.set_major_tick_every(random.randint(1, 100))
+            widget.set_label_show(True)
+            widget.set_text_src([c for _ in range(random.randint(1, 10)) for c in ascii_letters])
         return widget
 
     def create_slider(self, element) -> lv.slider:
@@ -348,6 +445,9 @@ class UiLoader:
             value = element["options"].get("value", 0)
             widget.set_range(range_min, range_max)
             widget.set_value(value, lv.ANIM.ON)
+        else:
+            widget.set_range(0, 100)
+            widget.set_value(random.randint(0, 100), lv.ANIM.ON)
         return widget
 
     def create_spangroup(self, element) -> lv.spangroup:
@@ -358,6 +458,22 @@ class UiLoader:
     def create_spinbox(self, element) -> lv.spinbox:
         # TODO Implement spinbox widget
         widget = lv.spinbox(lv.screen_active())
+        if "options" in element:
+            range_min = element["options"].get("range_min", 0)
+            range_max = element["options"].get("range_max", 100)
+            step = element["options"].get("step", 1)
+            value = element["options"].get("value", random.randint(0, 100))
+            widget.set_range(range_min, range_max)
+            widget.set_step(step)
+            widget.set_value(value)
+        else:
+            min = random.randint(0, 100)
+            max = random.randint(min, 100)
+            step = random.randint(1, 10)
+            value = random.randint(min, max)
+            widget.set_range(min, max)
+            widget.set_step(step)
+            widget.set_value(value)
         return widget
 
     def create_spinner(self, element) -> lv.spinner:
@@ -368,11 +484,25 @@ class UiLoader:
     def create_switch(self, element) -> lv.switch:
         # TODO Implement switch widget
         widget = lv.switch(lv.screen_active())
+        if "options" in element:
+            state = element["options"].get("state", False)
+            sw_state = getattr(lv.STATE, state.upper(), lv.STATE.DISABLED)
+            widget.add_state(sw_state)
+        else:
+            widget.add_state(lv.STATE.DEFAULT)
         return widget
     
     def create_table(self, element) -> lv.table:
         # TODO Implement table widget
         widget = lv.table(lv.screen_active())
+        if "options" in element:
+            col_cnt = element["options"].get("column_count", 1)
+            row_cnt = element["options"].get("row_count", 1)
+            widget.set_column_count(col_cnt)
+            widget.set_row_count(row_cnt)
+        else:
+            widget.set_column_count(random.randint(1, 10))
+            widget.set_row_count(random.randint(1, 10))
         return widget
 
     def create_tabview(self, element) -> lv.tabview:
@@ -383,7 +513,11 @@ class UiLoader:
     def create_textarea(self, element) -> lv.textarea:
         # TODO Implement textarea widget
         widget = lv.textarea(lv.screen_active())
-        # widget.set_text('Textarea')
+        if "options" in element:
+            text = element["options"].get("text", str([c for _ in range(random.randint(10, 100)) for c in ascii_letters]))
+            widget.set_text(text)
+        else:
+            widget.set_text(str([c for _ in range(random.randint(10, 100)) for c in ascii_letters]))
         return widget
     
     def create_tileview(self, element) -> lv.tileview:
@@ -500,7 +634,7 @@ class UiLoader:
 
 if __name__ == "__main__":
     # Load UI from JSON file
-    ui_loader = UiLoader("./designs/media_playback_example.json")
+    ui_loader = UiLoader("./designs/test_widget.json")
     ui_loader.initialize_screen()
     ui_loader.parse_ui()
     root_widget = ui_loader.get_root_widget()
