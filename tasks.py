f926509396002a412e8f2f7e7702f96fee10ea33
HackXIt <hackxit@gmail.com>
2024-04-01 03:54:55 +0200

    Update invoke tasks for either random or design

--- tasks.py
+++ tasks.py
@@ -11,9 +11,24 @@ lv_conf_original = os.path.join(os.path.curdir, 'lv_micropython', 'lib', 'lv_bin
 lv_conf_temp = os.path.join(os.path.curdir, 'lv_conf.tmp')
 
 @task
-def generate(ctx):
-    widget_list = ['button', 'label', 'line', 'bar', 'slider', 'switch', 'checkbox', 'roller', 'dropdown', 'textarea', 'chart']
-    subprocess.run([micropython, main, '-W', '420', '-H', '320', '-c', '1', '-t', ' '.join(widget_list), '-o', 'screenshot.raw', '-l', 'none'])
+def generate_random(ctx, widget_list: str = 'button label line bar slider switch checkbox roller dropdown textarea chart', width: int = 420, height: int = 320, count: int = 1, layout: str = 'none', output: str = 'screenshot.jpg', normalize: bool = False):
+    args = [micropython, main, '-m', 'generator', '-W', str(width), '-H', str(height), '-c', str(count), '-o', output, '-l', layout]
+    args.append('-t')
+    for widget in widget_list.split(' '):
+        args.append(widget)
+    if normalize:
+        args.append('--normalize')
+    subprocess.run(args)
+
+@task
+def generate_design(ctx, design_file: str ='designs/example.json', output: str = 'screenshot.jpg', normalize: bool = False):
+    if not os.path.exists(design_file):
+        print(f"Design file {design_file} does not exist.")
+        return
+    args = [micropython, main, '-m', 'design', '-f', design_file, '-o', output]
+    if normalize:
+        args.append('--normalize')
+    subprocess.run(args)
 
 @task
 def convert(ctx, width='420', height='320', input='screenshot.bin', output='screenshot.jpg'):
@@ -22,9 +37,9 @@ def convert(ctx, width='420', height='320', input='screenshot.bin', output='scre
 @task
 def sample(ctx, type='button', count='1', width='420', height='320', layout='none', output='screenshot.jpg', normalize: bool = False):
     if normalize:
-        gen = subprocess.run([micropython, main, '-W', width, '-H', height, '-c', count, '-t', type, '-o', output, '-l', layout, '-n'])
+        gen = subprocess.run([micropython, main, '-m', 'generator' '-W', width, '-H', height, '-c', count, '-t', type, '-o', output, '-l', layout, '-n'])
     else:
-        gen = subprocess.run([micropython, main, '-W', width, '-H', height, '-c', count, '-t', type, '-o', output, '-l', layout])
+        gen = subprocess.run([micropython, main, '-m', 'generator', '-W', width, '-H', height, '-c', count, '-t', type, '-o', output, '-l', layout])
     if gen.returncode == 0:
         print("UI sample generation successful.")
     else:
